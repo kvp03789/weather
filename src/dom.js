921c452fc951059558d044f22f71da6ab75f90af
clay <clay.nichols@gmail.com>
2022-10-20 00:29:48 -0500

    Add div sections within main block w dummy content

--- src/dom.js
+++ src/dom.js
@@ -1,24 +1,29 @@
-import { createDom } from './utils';
+import { createDom, createDomMultiple } from './utils';
 
 export function makeDom() {
     const mainBlock = createDom("div", "main-block");
     
     const leftBlock = createDom("div", "left-block");
     const rightBlock = createDom("div", "right-block");
+    const topBlock = createDom("div", "top-block");
+    const bottomBlock = createDom("div", "bottom-block");
 
     const tempMainTitle = createDom("h1", "title");
     const weatherMainTitle = createDom("h1", "title");
     const citySubTitle = createDom("h3", "sub-title");
     const weatherSubTitle = createDom("h3", "sub-title");
+    const infoDivs = createDomMultiple("div", 6, bottomBlock, "info");
+    const unitsButton = createDom("div", "round-button")
 
-    tempMainTitle.innerText = "40";
+    tempMainTitle.innerText = "40°";
     citySubTitle.innerText = "Jasper, AL";
     weatherMainTitle.innerText = "Clear";
     weatherSubTitle.innerText = "clear sky";
-
+    unitsButton.innerText = "C°";
 
     leftBlock.append(tempMainTitle, citySubTitle);
     rightBlock.append(weatherMainTitle, weatherSubTitle);
-    mainBlock.append(leftBlock, rightBlock);
+    topBlock.append(leftBlock, rightBlock);
+    mainBlock.append(topBlock, bottomBlock, unitsButton);
     document.querySelector(".container").append(mainBlock);
 }
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,17 @@ export function createDom(ele, ...className) {
     newElement.classList.add(className);
     return newElement;
 }
+
+export function createDomMultiple(ele, num, element, ...className) {
+    for (let i = 0; i < num; i++) {
+        const newElement = document.createElement(ele);
+        newElement.classList.add(className);
+        element.append(newElement);
+        const infoWeatherTop = createDom("h3", "info-top");
+        const infoWeatherBottom = createDom("h3", "info-bottom");
+        newElement.append(infoWeatherTop, infoWeatherBottom);
+        infoWeatherTop.innerText = "Humidity";
+        infoWeatherBottom.innerText = "36%";
+    }
+    
+}
